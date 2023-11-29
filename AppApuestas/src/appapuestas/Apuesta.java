87f4411f3a0fa019639be2f032a5453123b7436f
Kevin <Kevin@ORDENA-PC>
2023-11-29 23:45:35 +0100

    commit

---
+++ AppApuestas/src/appapuestas/Apuesta.java
@@ -0,0 +1,120 @@
+package appapuestas;
+
+public class Apuesta {
+	
+	//ATRIBUTOS A NIVEL DE CLASE
+	public static String equipoLocal;
+	public static String equipoVisitante;
+	
+	
+	
+	//ATRIBUTOS A NIVEL DE OBJETO
+	private String nombre;
+	//Resultado
+	private int golesLocal;
+	private int golesVisitante;
+	private float valorApuesta;
+	
+	//SECCIÓN QUE SIRVE PARA INICIALIZAR
+	//ATRIBUTOS DE LA CLASE
+	static {
+		//sección de código que se ejecuta al inicio
+		//automáticamente, cuando Java lee 
+		//el nombre de la clase por primera vez
+		// en el código
+		
+		Apuesta.equipoLocal = "Real Madrid";
+		Apuesta.equipoVisitante = "Barcelona";
+		
+		//System.out.println("En la sección static de Apuesta");
+		
+	}
+	
+	
+	
+	public static String getEquipoLocal() {
+		return equipoLocal;
+	}
+	
+	public static void setEquipoLocal(String equipoLocal) {
+		Apuesta.equipoLocal = equipoLocal;
+	}
+	
+	public static String getEquipoVisitante() {
+		return equipoVisitante;
+	}
+	
+	public static void setEquipoVisitante(String equipoVisitante) {
+		Apuesta.equipoVisitante = equipoVisitante;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getGolesLocal() {
+		return golesLocal;
+	}
+	
+	public void setGolesLocal(int golesLocal) {
+		this.golesLocal = golesLocal;
+	}
+	
+	public int getGolesVisitante() {
+		return golesVisitante;
+	}
+	
+	public void setGolesVisitante(int golesVisitante) {
+		this.golesVisitante = golesVisitante;
+	}
+
+	public float getValorApuesta() {
+		return valorApuesta;
+	}
+
+	public void setValorApuesta(float valorApuesta) {
+		this.valorApuesta = valorApuesta;
+	}
+
+	public Apuesta(String nombre, int golesLocal, int golesVisitante) {
+		super();
+		this.nombre = nombre;
+		this.golesLocal = golesLocal;
+		this.golesVisitante = golesVisitante;
+	}
+	
+	public Apuesta() {
+		// TODO Auto-generated constructor stub
+	}
+    
+	public Apuesta(String nombre, int golesLocal, int golesVisitante, float valorApuesta) {
+		super();
+		this.nombre = nombre;
+		this.golesLocal = golesLocal;
+		this.golesVisitante = golesVisitante;
+		this.valorApuesta = valorApuesta;
+	}
+
+	@Override
+	public String toString() {
+		return "Apuesta = Nombre:" + this.nombre + ", Goles equipo local = " + this.golesLocal + ", Goles equipo visitante = " + this.golesVisitante + ", Cantidad Apostada = " + this.valorApuesta + " €";
+	}
+	
+	public void mostrarApuesta ()
+	{
+		//Alison 1-0
+		System.out.println(this.nombre + " " + this.golesLocal+"-"+this.golesVisitante +" " +this.valorApuesta);
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ AppApuestas/src/appapuestas/MainApuestas.java
@@ -0,0 +1,293 @@
+package appapuestas;
+
+import java.util.Scanner;
+
+import appapuestas.Apuesta;
+
+public class MainApuestas {
+
+	// TODO Controlar que un apostador sólo pueda
+	// registrar una apuesta
+
+	public static final int MAXIMO_APUESTAS = 20;// AMIBTO GLOBAL
+
+	public static Apuesta crearApuesta() {
+		Apuesta apuesta = null;
+
+		System.out.println("Registrar apuestas");
+		apuesta = new Apuesta();
+		System.out.println("Introduce tu nombre");
+		Scanner scanner = new Scanner(System.in);
+		String nombreapostante = scanner.next();
+
+		System.out.println("Número de goles " + Apuesta.equipoLocal);
+		int goleslocal = scanner.nextInt();
+
+		System.out.println("Número de goles " + Apuesta.equipoVisitante);
+		int golesvisitante = scanner.nextInt();
+		
+		System.out.println("Valor de la apuesta");
+		float valorapuesta = scanner.nextFloat();
+		
+		apuesta.setNombre(nombreapostante);
+		apuesta.setGolesLocal(goleslocal);
+		apuesta.setGolesVisitante(golesvisitante);
+		apuesta.setValorApuesta(valorapuesta);
+
+		return apuesta;
+	}
+
+	public static void mostrarMenu() {
+
+		
+		System.out.println("1. Mostrar Apuestas");
+		System.out.println("2. Registrar Nueva Apuesta");
+		System.out.println("3. Borrar apuestas (todas)");
+		System.out.println("4. Buscar apostante");
+		System.out.println("5. Mostrar estadísticas");
+		System.out.println("6. Salir");
+
+		//System.out.print("Selecciona una opción: ");
+
+	}
+
+	public static int leerOpcion() {
+
+		int opcionleida = 0;
+		Scanner scanner = null;
+
+		scanner = new Scanner(System.in);
+		opcionleida = scanner.nextInt();
+
+		return opcionleida;
+	}
+
+	/**
+	 * Método descubre sí el nombre de apostante ya existe
+	 * @param arayApuestas el registro de apuestas donde buscar
+	 * @param nombre       el nombre del apostador
+	 * @param numApuestas  la cima/ el número de apuestas del Array
+	 * @return false si el nombre no está en el array o tur si sí
+	 */
+	public static boolean esApuestaRepetida(Apuesta[] arrayApuestas, String nombre, int numApuestas) {
+		boolean nombreRepetido = false;
+		int apuestaActual = 0;
+		Apuesta apuestaAux = null;
+		String nombreActual = null;
+
+		while ((apuestaActual < numApuestas) && (!nombreRepetido)) {
+			// obtengo la apuesta
+			apuestaAux = arrayApuestas[apuestaActual];
+			// comparo el nombre
+			nombreActual = apuestaAux.getNombre();
+			if (nombreActual.equals(nombre)) {
+				nombreRepetido = true;
+			}
+			apuestaActual++;
+
+		}
+
+		return nombreRepetido;
+	}
+
+	/**
+	 * Método que nos busca una apuesta dado el nombre
+	 * del apostador
+	 * @param arrayApuestas el array de las apuestras
+	 * @param nombreApostadorBuscado el nombre del apostador a buscar
+	 * @param numApuestas las apuestas que tengo en el array
+	 * @return null si no existe un apostador con ese nombre
+	 * o la apuesta si ese apostador está
+	 */
+	public static Apuesta bucarApuesta(Apuesta[] arrayApuestas, String nombreApostadorBuscado, int numApuestas) 
+	{
+
+		Apuesta apuesta = null;
+		boolean nombreEncontrado = false;
+		Apuesta apuestaAux = null;
+		String nombreAux = null;
+
+		int i = 0;
+		while ((i < numApuestas) && (!nombreEncontrado)) {
+			apuestaAux = arrayApuestas[i];
+			nombreAux = apuestaAux.getNombre();
+			if (nombreAux.equals(nombreApostadorBuscado)) {
+				nombreEncontrado = true;
+				apuesta = arrayApuestas[i];
+			}
+			i++;
+		}
+
+		return apuesta;
+	}
+	
+	
+	//HACER ESTADISTICA DE LA MEDIA
+
+	public static float estadisticaMediaApuesta(Apuesta[] arrayApuestas, int numApuestas) {
+
+		float promedio = 0;
+		float suma = 0;
+
+		if (numApuestas > 0) {
+
+			for (int i = 0; i < numApuestas; i++) {
+				suma = suma + arrayApuestas[i].getValorApuesta();
+				
+			}
+			promedio = suma / numApuestas;
+		}
+		
+		return promedio;
+	}
+
+	// HACER ESTADISTICA DEL MAYOR DE APUESTAS
+
+	public static float estadisticaMayorApuesta(Apuesta[] arrayApuestas, int numApuestas) {
+
+		float mayorApuesta = 0;
+
+		if (numApuestas > 0) {
+
+			for (int i = 0; i < numApuestas; i++) {
+				if (arrayApuestas[i].getValorApuesta() > mayorApuesta ) {
+					mayorApuesta = arrayApuestas[i].getValorApuesta();
+				}
+			}
+		}
+		return mayorApuesta;
+	}
+	
+	public static float estadisticaMenorApuesta(Apuesta[] arrayApuestas, int numApuestas) {
+		float menor = Float.MAX_VALUE;//pongo el níumero más grande, para que entre en el menor
+		
+		//menor  = (numApuestas > 0) ? Float.MAX_VALUE : 0;
+		
+		if (numApuestas > 0) {
+		
+			for (int i = 0; i < numApuestas; i++) {
+				if (arrayApuestas[i].getValorApuesta() < menor) {
+					menor = arrayApuestas[i].getValorApuesta();
+				}
+			}
+			
+		} else {
+			//si no tengo apuestas, la menor es cero
+			menor = 0;
+		}
+
+		
+
+		return menor;
+	}
+
+	/** 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		boolean salir = false;// controlo el final del programa
+		Apuesta[] arrayApuestas = new Apuesta[MAXIMO_APUESTAS];
+		int numApuestas = 0;// lo uso para llevar la cuenta de apuestas
+		// TODO AÑADIR LA OPCIÓN DE BORRAR APUESTAS (TODAS)
+		int opcionleida = 0;
+		do {
+			mostrarMenu();
+			opcionleida = leerOpcion();
+			System.out.println("El usuario ha elegido la opcion " + opcionleida);
+
+			switch (opcionleida) {
+			case 1:
+
+				if (numApuestas > 0) {
+					Apuesta apuestaActual = null;
+					// for(Apuesta apuesta : arrayApuestas)
+					for (int i = 0; i < numApuestas; i++) {
+						apuestaActual = arrayApuestas[i];
+						System.out.println(apuestaActual.toString());
+					}
+				} else {
+					System.out.println("NO hay apuestas");
+				}
+
+				System.out.println("mostrar apuestas");
+				break;
+
+			case 2:// REGISTRAR APUESTA
+
+				if (numApuestas < MAXIMO_APUESTAS) {
+					Apuesta apuestaNueva = crearApuesta();
+					System.out.println("Apuesta creada ");
+
+					boolean apuestaRepe = esApuestaRepetida(arrayApuestas, apuestaNueva.getNombre(), numApuestas);
+					if (apuestaRepe) {
+						System.out.println(apuestaNueva.getNombre() + " Ya ha apostado");
+					} else {
+						// guardo
+						apuestaNueva.mostrarApuesta();
+						arrayApuestas[numApuestas] = apuestaNueva;
+						numApuestas++;
+					}
+
+				} else {
+					System.out.println("APUESTAS CERRADAS");
+				}
+
+				break;
+
+			case 3:
+				for (int i = 0; i < numApuestas; i++) {
+					arrayApuestas[i] = null;
+
+				}
+				numApuestas = 0;// actualizo el numero de apuestas el cero!
+				System.out.println("Apuestas BORRADAS");
+				break;
+
+			case 4:
+				System.out.println("Ingresa el nombre del apostador que deseas buscar:");
+				Scanner scanner = new Scanner(System.in);
+				String nombreBuscar = scanner.next();
+				Apuesta apuestaEncontrada = bucarApuesta(arrayApuestas, nombreBuscar, numApuestas);
+
+				if (apuestaEncontrada != null) {
+					System.out.println("APOSADOR ENCONTRADO");
+					apuestaEncontrada.mostrarApuesta();
+
+				} else {
+					System.out.println("APOSTADOR NO ENCONTRADO");
+				}
+
+				break;
+
+			case 5:
+				System.out.println("generar estadísticas");
+				//salir = true;
+				
+				//TODO calcular media
+				float media = estadisticaMediaApuesta(arrayApuestas, numApuestas);
+				System.out.println("La media de apuestas es " + media);
+				//TODO calcular maximo
+				float mayor = estadisticaMayorApuesta(arrayApuestas, numApuestas);
+				System.out.println("La mayor de las apuestas es " + mayor);
+				//TODO calcular mínimo
+				float menor = estadisticaMenorApuesta(arrayApuestas, numApuestas);
+				System.out.println("La menor de las apuestas es " + menor);
+				
+			
+				break;	
+			case 6:
+				System.out.println("salir");
+				salir = true;
+				break;
+
+			default:
+				// throw new IllegalArgumentException("Unexpected value: " + opcionleida);
+				System.out.println("Opción incorrecta mete del 1 al 3");
+			}
+
+		} while (!salir); // (opcionleida != 4);// mientras no seleccione la opcion 3
+		System.out.println("PROGRAMA TERMINADO");
+	}
+}
